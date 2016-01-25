d897235597c3175fba44638dce4267941d828dbe
Jose Méndez <josecarlos@windowslive.com>
2016-01-24 20:13:53 -0500

    Active and resolved user stories.

--- out/dashboard.js
+++ out/dashboard.js
@@ -1,20 +1,44 @@
 /// <reference path='../sdk/VSS' />
 define(["require", "exports", "VSS/Controls", "VSS/Controls/Grids"], function (require, exports, Controls, Grids) {
+    var resolvedUserStories, activeUserStories;
+    var projectId;
+    var witClient;
     var container = $("#grid-container");
-    var gridOptions = {
-        height: "400px",
-        width: "40%",
-        source: function () {
-            var result = [], i;
-            for (i = 0; i < 100; i++) {
-                result[result.length] = [i, "Contenido de prueba"];
-            }
-            return result;
-        }(),
-        columns: [
-            { text: "Columna 1", index: 0, width: 200 },
-            { text: "Columna 2", index: 1, width: 200 }]
-    };
-    Controls.create(Grids.Grid, container, gridOptions);
-    VSS.notifyLoadSucceeded();
+    VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], function (VSS_Service, TFS_Wit_WebApi) {
+        projectId = VSS.getWebContext().project.id;
+        witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+        retrieveResolvedUserStories();
+    });
+    function retrieveResolvedUserStories() {
+        var query = { query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Resolved') AND [System.WorkItemType] = 'User Story'" };
+        witClient.queryByWiql(query, projectId).then(function (result) {
+            resolvedUserStories = result.workItems.length;
+            retrieveActiveUserStories();
+        });
+    }
+    function retrieveActiveUserStories() {
+        var query = { query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Active') AND [System.WorkItemType] = 'User Story'" };
+        witClient.queryByWiql(query, projectId).then(function (result) {
+            activeUserStories = result.workItems.length;
+            buildGrid();
+        });
+    }
+    function buildGrid() {
+        var options = {
+            width: "50%",
+            height: "500px",
+            source: function () {
+                var data = [];
+                data.push(["Resolved user stories", resolvedUserStories]);
+                data.push(["Active user stories", activeUserStories]);
+                return data;
+            }(),
+            columns: [
+                { text: "", index: 0, width: 250 },
+                { text: "", index: 1, width: 250 }
+            ]
+        };
+        Controls.create(Grids.Grid, container, options);
+        VSS.notifyLoadSucceeded();
+    }
 });
--- scripts/dashboard.ts
+++ scripts/dashboard.ts
@@ -2,24 +2,61 @@
 
 import Controls = require("VSS/Controls");
 import Grids = require("VSS/Controls/Grids");
+import TFS_Wit_WebApi = require("TFS/WorkItemTracking/RestClient");
+import VSS_Service = require("VSS/Service");
 
+
+var resolvedUserStories, activeUserStories;
+var projectId;
+var witClient;
 var container = $("#grid-container");
-var gridOptions: Grids.IGridOptions = {
-    height: "400px",
-    width: "40%",
-    source: function () {
-        var result = [], i;
-        for (i = 0; i < 100; i++) {
-            result[result.length] = [i, "Contenido de prueba"];
-        }
 
-        return result;
-    } (),
-    columns: [
-        { text: "Columna 1", index: 0, width: 200 },
-        { text: "Columna 2", index: 1, width: 200}]
-};
 
-Controls.create<Grids.Grid, Grids.IGridOptions>(Grids.Grid, container, gridOptions);
 
-VSS.notifyLoadSucceeded();
+VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"],  
+    function (VSS_Service, TFS_Wit_WebApi) {
+        projectId = VSS.getWebContext().project.id;
+        witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+        retrieveResolvedUserStories();
+});
+
+
+function retrieveResolvedUserStories() : void {
+    var query = {query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Resolved') AND [System.WorkItemType] = 'User Story'"};
+    witClient.queryByWiql(query, projectId).then(function (result) {
+        resolvedUserStories = result.workItems.length;
+        retrieveActiveUserStories();
+    });
+}
+
+function retrieveActiveUserStories() : void {
+    var query = {query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Active') AND [System.WorkItemType] = 'User Story'"};
+    witClient.queryByWiql(query, projectId).then(function (result) {
+        activeUserStories = result.workItems.length;
+        buildGrid();
+    });
+}
+
+
+function buildGrid(): void {
+    var options = {
+        width: "50%",
+        height: "500px",
+        source: function () {
+            var data = [];
+            data.push(["Resolved user stories", resolvedUserStories]);
+            data.push(["Active user stories", activeUserStories]);
+            return data;
+        }(),
+        columns: [
+            { text: "", index: 0, width: 250 },
+            { text: "", index: 1, width: 250 }
+        ]
+    };
+    Controls.create<Grids.Grid, Grids.IGridOptions>(Grids.Grid, container, options);
+    VSS.notifyLoadSucceeded();
+}
+
+
+
+
