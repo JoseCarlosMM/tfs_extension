276ada0535b562c9980c5f1dece97e5bf46dc1b8
jarodriguez08 <jarc2693@hotmail.com>
2016-02-03 12:13:05 -0600

    This handle the PBI and Bug section.

---
+++ out/PBIs.js
@@ -0,0 +1,60 @@
+/// <reference path='../sdk/VSS' />
+define(["require", "exports", "VSS/Controls", "VSS/Controls/Grids"], function (require, exports, Controls, Grids) {
+    var approvedPBI, committedPBI, donePBI, openImpediments;
+    var projectId;
+    var witClient;
+    var container = $("#PBI");
+    VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"], function (VSS_Service, TFS_Wit_WebApi) {
+        projectId = VSS.getWebContext().project.id;
+        witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+        retrieveApprovedPBI();
+    });
+    function retrieveApprovedPBI() {
+        var query = { query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Approved') AND [System.WorkItemType] = 'Product Backlog Item'" };
+        witClient.queryByWiql(query, projectId).then(function (result) {
+            approvedPBI = result.workItems.length;
+            retrieveCommittedPBI();
+        });
+    }
+    function retrieveCommittedPBI() {
+        var query = { query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Committed') AND [System.WorkItemType] = 'Product Backlog Item'" };
+        witClient.queryByWiql(query, projectId).then(function (result) {
+            committedPBI = result.workItems.length;
+            retrieveDonePBI();
+        });
+    }
+    function retrieveDonePBI() {
+        var query = { query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Done') AND [System.WorkItemType] = 'Product Backlog Item'" };
+        witClient.queryByWiql(query, projectId).then(function (result) {
+            donePBI = result.workItems.length;
+            buildGrid();
+        });
+    }
+    function retrieveOpenImpediments() {
+        var query = { query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Open') AND [System.WorkItemType] = 'Impediment'" };
+        witClient.queryByWiql(query, projectId).then(function (result) {
+            openImpediments = result.workItems.length;
+            buildGrid();
+        });
+    }
+    function buildGrid() {
+        var options = {
+            width: "100%",
+            height: "80%",
+            source: function () {
+                var data = [];
+                data.push(["Approved Product Backlog Items", approvedPBI]);
+                data.push(["Committed Product Backlog Items", committedPBI]);
+                data.push(["Product Backlog Items Done", donePBI]);
+                data.push(["Open Impediments", openImpediments]);
+                return data;
+            }(),
+            columns: [
+                { text: "Id.", index: 0, width: 250 },
+                { text: "No.", index: 1, width: 250 }
+            ]
+        };
+        Controls.create(Grids.Grid, container, options);
+        VSS.notifyLoadSucceeded();
+    }
+});
---
+++ scripts/PBIs.ts
@@ -0,0 +1,74 @@
+ /// <reference path='../sdk/VSS' />
+
+import Controls = require("VSS/Controls");
+import Grids = require("VSS/Controls/Grids");
+import TFS_Wit_WebApi = require("TFS/WorkItemTracking/RestClient");
+import VSS_Service = require("VSS/Service");
+
+
+var approvedPBI, committedPBI, donePBI, openImpediments;
+var projectId;
+var witClient;
+var container = $("#PBI");
+
+
+
+VSS.require(["VSS/Service", "TFS/WorkItemTracking/RestClient"],  
+    function (VSS_Service, TFS_Wit_WebApi) {
+        projectId = VSS.getWebContext().project.id;
+        witClient = VSS_Service.getCollectionClient(TFS_Wit_WebApi.WorkItemTrackingHttpClient);
+        retrieveApprovedPBI();
+});
+
+function retrieveApprovedPBI() : void {
+    var query = {query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Approved') AND [System.WorkItemType] = 'Product Backlog Item'"};
+    witClient.queryByWiql(query, projectId).then(function (result) {
+        approvedPBI = result.workItems.length;
+        retrieveCommittedPBI();
+    });
+}
+
+function retrieveCommittedPBI() : void {
+    var query = {query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Committed') AND [System.WorkItemType] = 'Product Backlog Item'"};
+    witClient.queryByWiql(query, projectId).then(function (result) {
+        committedPBI = result.workItems.length;
+        retrieveDonePBI();
+    });
+}
+
+function retrieveDonePBI() : void {
+    var query = {query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Done') AND [System.WorkItemType] = 'Product Backlog Item'"};
+    witClient.queryByWiql(query, projectId).then(function (result) {
+        donePBI = result.workItems.length;
+        buildGrid();
+    });
+}
+
+function retrieveOpenImpediments() : void {
+    var query = {query: "SELECT [System.Id] FROM WorkItem WHERE [System.State] IN ('Open') AND [System.WorkItemType] = 'Impediment'"};
+    witClient.queryByWiql(query, projectId).then(function (result) {
+        openImpediments = result.workItems.length;
+        buildGrid();
+    });
+}
+
+function buildGrid(): void {
+    var options = {
+        width: "100%",
+        height: "80%",
+        source: function () {
+            var data = [];
+            data.push(["Approved Product Backlog Items", approvedPBI]);
+            data.push(["Committed Product Backlog Items", committedPBI]);
+            data.push(["Product Backlog Items Done", donePBI]);
+            data.push(["Open Impediments", openImpediments]);
+            return data;
+        }(),
+        columns: [
+            { text: "Id.", index: 0, width: 250 },
+            { text: "No.", index: 1, width: 250 }
+        ]
+    };
+    Controls.create<Grids.Grid, Grids.IGridOptions>(Grids.Grid, container, options);
+    VSS.notifyLoadSucceeded();
+}
